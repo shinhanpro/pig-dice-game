18ec9509fdad506aaed168e4e65e554d32d3b1ad
yewonbahn <banyaewon98@gmail.com>
2023-01-13 14:38:58 +0900

    #5 feat: Add user_loop_game def

--- play_game.py
+++ play_game.py
@@ -6,6 +6,27 @@ def play_game(n,target_score):
     users_score=[0]*n # 사용자 수 만큼 유저 점수 리스트 생성
     i=0
     check=False
+
+    def user_loop_game(target_score):
+        nonlocal winner
+        nonlocal check
+        while True:
+            user_pick= input("enter roll or bank: ")#roll or bank 입력 받기
+            if user_pick=="roll":
+                roll=randint(1,6)
+                print(f"주사위 값은 {roll}입니다.")
+                if roll==1:
+                    users_score[turn]=0   
+                    break
+                else:
+                    users_score[turn]+=roll
+                    if users_score[turn]>=target_score:
+                        winner=turn  
+                        check=True
+                        break             
+            elif user_pick=="bank":
+                break
+               
     while True:
         if check==True: #승자가 발생하면?
             print(f"winner는 {winner}입니다 (0=user,1~n=com)")
