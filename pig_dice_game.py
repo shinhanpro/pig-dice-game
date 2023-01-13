e7d440ab0e268a4fa54ae64e51e7414879aefb6c
0andme <yu00mi97@gmail.com>
2023-01-13 14:07:38 +0900

    #4 feat : 사용자로부터 참여 인원 입력 받기
    - 1. 사용자로부터 참여 인원을 입력 받기
    - 2. 숫자입력 예외처리 기능
    - 3. 1과 2의 과정 반복

--- pig_dice_game.py
+++ pig_dice_game.py
@@ -1,13 +1,25 @@
-def game_opening():
-    print_char_repeat('-',40)
-    print(f"{'Pig Dice Game에 오신걸 환영합니다':^32}")
-    print_char_repeat('-',40)
-
 def print_char_repeat(char,num):
     chars=''
     for _ in range(num):
         chars+=char
     print(chars)
+
+
+def user_input_n():
+    try :
+         n=int(input("참여할 인원을 입력해주세요 ex) 7 > "))
+         return n
+    except ValueError:
+        print('* 숫자만 입력해주세요\n')
+        user_input_n()
+
+
+def game_opening():
+    print_char_repeat('-',40)
+    print(f"{'Pig Dice Game에 오신걸 환영합니다':^32}")
+    print_char_repeat('-',40)
+
+    n=user_input_n()
 
 
 
